4520328371c6924851d47f9b199260c317ed5667
ChipJust <chipjust@gmail.com>
2016-06-18 16:48:59 -0700

    Reorder parameters to match ngspice documentation order Add some ltspice specific parameters Refactor paramter lists to be dict with defaults Fix bug for negative parameter values

--- diodes.py
+++ diodes.py
@@ -4,28 +4,49 @@ Copyright (c) 2016 Chip Ueltschey
 Released under the terms of MIT license (see LICENSE for details)
 
 Name    Parameter                                       Units   Default
-IS      Saturation current (diode equation)             A       1E-14
-RS      Parsitic resistance (series resistance)         ohm     0
+BV      Reverse breakdown voltage                       V       infinite
+IBV     Reverse breakdown current                       A       1E-3
+IK (IKF) Forward knee current                           A       1E-3
+IKR     Reverse knee current                            A       1E-3
+IS (JS) Saturation current (diode equation)             A       1E-14
+JSW     Sidewall saturation current                     A       1E-14
 N       Emission coefficient, 1 to 2                    -       1
-TT      Transit time                                    s       0
+RS      Parsitic resistance (series resistance)         ohm     0
+
 CJO     Zero-bias junction capacitance                  F       0
-VJ      Junction potential                              V       1
+CJP     Zero-bias junction sidewall capacitance         F       0
+FC      Forward bias depletion capacitance coefficient  -       0.5
+FCS     Coefficient for forward-bias depletion sidewall capacitance formula -   0.5
 M       Junction grading coefficient                    -       0.5
 -       0.33 for linearly graded junction               -       -
 -       0.5 for abrupt junction                         -       -
+MJSW    Periphery junction grading coefficient          -       0.33
+VJ      Junction potential                              V       1
+PHP     Periphery junction potential                    V       1
+TT      Transit time                                    s       0
+
 EG      Activation energy:                              eV      1.11
 -       Si: 1.11                                        -       -
 -       Ge: 0.67                                        -       -
 -       Schottky: 0.69                                  -       -
+TNOM (TREF) Parameter measurement temperature           C       27
 XTI     IS temperature exponent                         -       3.0
 -       pn junction: 3.0                                -       -
 -       Schottky: 2.0                                   -       -
+
 KF      Flicker noise coefficient                       -       0
 AF      Flicker noise exponent                          -       1
-FC      Forward bias depletion capacitance coefficient  -       0.5
-BV      Reverse breakdown voltage                       V       infinite
-IBV     Reverse breakdown current                       A       1E-3
 
+NR
+ISR
+-- LT Spice Power Parameters --
+VPK     Peak voltage rating
+IPK     Peak current rating
+IAVE    Ave current rating
+IRMS    RMS current rating
+DISS    Maximum power dissipation rating
+NBVL
+IBVL
 """
 import re
 import io
@@ -34,57 +55,78 @@ import math
 class SpiceDiode():
     re_model_paramter_clean = re.compile(r"\n\s*\+")
     re_parameters = re.compile(r"\s*(?P<attribute>\S+)\s*=\s*(?P<value>\S+)\s*")
-    spice_parameters = [
-        "IS",
-        "RS",
-        "N",
-        "TT",
-        "CJO",
-        "VJ",
-        "M",
-        "EG",
-        "XTI",
-        "KF",
-        "AF",
-        "FC",
-        "BV",
-        "IBV",
-    ]
-    infomational_parameters = [
-        "MFG",
-        "TYPE",
-    ]
+    spice_parameters = { # All values must be coersible to float
+        "BV"    :   1E100,
+        "IBV"   :   1E-3,
+        "IK"    :   1E-3,
+        "IKR"   :   1E-3,
+        "IS"    :   1E-14,
+        "JSW"   :   1e-14,
+        "N"     :   1,
+        "RS"    :   0,
+        "CJO"   :   0,
+        "CJP"   :   0,
+        "FC"    :   0.5,
+        "FCS"   :   0.5,
+        "M"     :   0.5,
+        "MJSW"  :   0.33,
+        "VJ"    :   1,
+        "PHP"   :   1,
+        "TT"    :   0,
+        "EG"    :   1.11,
+        "TNOM"  :   27,
+        "XTI"   :   3.0,
+        "KF"    :   0,
+        "AF"    :   1,
+        "VPK"   :   0,
+        "IPK"   :   0,
+        "IAVE"  :   0,
+        "IRMS"  :   0,
+        "DISS"  :   0,
+    }
+    infomational_parameters = { # All values must be coersible to string
+        "MFG"   :   "",
+        "TYPE"  :   "",
+    }
+    parameter_alias = {
+        "CJ0"   :   "CJO",
+        "MJ"    :   "M",
+        "IKF"   :   "IK",
+        "JS"    :   "IS",
+        "CJSW"  :   "CJP",
+        "PB"    :   "VJ",
+        "TREF"  :   "TNOM",
+    }
     def __init__(self, name, parameters):
         self.name=name
-        #Defaults
-        self.IS = 1E-14
-        self.RS = 0
-        self.N = 1
-        self.TT = 0
-        self.CJO = 0
-        self.VJ = 1
-        self.M = .5
-        self.EG = 1.11
-        self.XTI = 3.0
-        self.KF = 0
-        self.AF = 1
-        self.FC = 0.5
-        self.BV = 1E100
-        self.IBV = 1E-3
+        # Set defaults
+        for p, v in self.spice_parameters.items():
+            setattr(self, p, v)
+        for p, v in self.infomational_parameters.items():
+            setattr(self, p, v)
+        # Parse parameters, override defaults
         for m in self.re_parameters.finditer(parameters):
             parameter = m.group('attribute').upper()
+            value = m.group('value')
             if parameter in self.infomational_parameters:
-                setattr(self, parameter, m.group('value'))
+                setattr(self, parameter, value)
                 continue
             if parameter not in self.spice_parameters:
-                print ("Ignoring parameter %s in %s." % (parameter, name))
-                continue
-            setattr(self, parameter, self.float(m.group('value')))
+                if parameter in self.parameter_alias:
+                    parameter = self.parameter_alias[parameter]
+                else:
+                    print ("Ignoring parameter %s=%s in %s." % (parameter, value, name))
+                    continue
+            try:
+                setattr(self, parameter, self.float(value))
+            except ValueError:
+                print ("Error parsing diode named %s: could not covert a paramter to float.\n  %s=%s" % (self.name, parameter, value))
+                raise
     def __repr__(self):
         return "SpiceDiode(%s, %s)" % (self.name, " ".join(["%s=%s" % (p, getattr(self, p, None)) for p in self.spice_parameters]))
-    re_scientific_notation = re.compile("\s*(\d*\.?\d*E-?\d*)[AVFH]?\s*", re.I)
-    re_scale_notation = re.compile("\s*(?P<number>\d*\.?\d*)(?P<scale>(?:t)|(?:g)|(?:x)|(?:meg)|(?:k)|(?:m)|(?:u)|(?:n)|(?:p)|(?:f))[AVFH]?\s*", re.I)
-    re_unit = re.compile("\s*(?P<number>\d*\.?\d*)[AVFH]?\s*", re.I)
+    re_scientific_notation = re.compile("\s*(-?\d*\.?\d*E-?\d*)[AVFH]?\s*", re.I)
+    re_scale_notation = re.compile("\s*(?P<number>-?\d*\.?\d*)(?P<scale>(?:t)|(?:g)|(?:x)|(?:meg)|(?:k)|(?:m)|(?:u)|(?:n)|(?:p)|(?:f))[AVFH]?\s*", re.I)
+    re_unit = re.compile("\s*(?P<number>-?\d*\.?\d*)[AVFH]?\s*", re.I)
     scales = {
         't' : 12,
         'g' : 9,
@@ -116,7 +158,10 @@ class SpiceDiode():
             return float(m.group(1))
         m = self.re_scale_notation.match(x)
         if m:
-            return float(m.group('number')) * math.pow(10, self.scales[m.group('scale')])
+            scale = m.group('scale').lower()
+            if scale not in self.scales:
+                print ("Parse error for diode named %s: %s not in scales" % (self.name, scale))
+            return float(m.group('number')) * math.pow(10, self.scales[scale])
         m = self.re_unit.match(x)
         if m:
             return float(m.group('number'))
@@ -166,10 +211,11 @@ test_data = [
 ]
 
 def main():
-    diodes = SpiceDiode.parse(test_data)
+    with open (r"E:\eda\diodes.cir") as f:
+        diodes = SpiceDiode.parse(f)
     
-    for diode in diodes:
-        print (diode)
+    #for diode in diodes:
+    #    print (diode)
 
 if __name__ == "__main__":
     main()
